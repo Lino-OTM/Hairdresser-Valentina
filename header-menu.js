7abc9eddc831896e4c660ebef7be032c6c2d4a3f
Ivan De Leon <ivandeleon_2301@hotmail.com>
2022-03-16 16:07:39 -0300

    Terminado el calendario

--- header-menu.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName("hamburger-menu")[0]
-const navbarLinks = document.getElementsByClassName("nav__list")[0]
-
-toggleButton.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active")
-})
